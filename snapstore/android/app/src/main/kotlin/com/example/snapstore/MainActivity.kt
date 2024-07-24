8ae21f10086a28830eb1b6060aa00689631844b5
JENIPHER <jeniperkuki@gmail.com>
2024-07-24 12:46:33 +0300

    file restracture

--- snapstore/android/app/src/main/kotlin/com/example/snapstore/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.snapstore
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- snapstore/lib/utils/constraints/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class TColor {
-  TColor._();
-
-  static Color primaryColor = Color(0xFF4B68FF);
-}
