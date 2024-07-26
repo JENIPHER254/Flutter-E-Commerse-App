cfe53694b9f8bd7be0353d09df7ddda30e9e4d3a
JENIPHER <jeniperkuki@gmail.com>
2024-07-26 16:27:04 +0300

    theme test

--- snapstore/lib/utils/theme/theme.dart
+++ snapstore/lib/utils/theme/theme.dart
@@ -1,8 +1,23 @@
 import "package:flutter/material.dart";
+import "package:snapstore/utils/theme/customTheme/textTheme.dart";
 
 class AppTheme {
   AppTheme._();
 
-  static ThemeData LightTheme = ThemeData();
-  static ThemeData DarkTheme = ThemeData();
+  static ThemeData LightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Crimson_Text",
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+  );
+  static ThemeData DarkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Crimson_Text",
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+  );
 }
